54ba05047a4842a33d5c709a77f69923346a5fef
Moritz Stapelfeldt <mstravendev@gmail.com>
2026-07-08 09:31:47 +0200

    Renaming and adjusting

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -17,7 +17,7 @@ const serveopts = {
 export default {
   input: 'src/main.js',
   output: {
-    file: 'dist/weather-chart-card.js',
+    file: 'dist/forecast-weather-chart-card.js',
     format: 'cjs',
     name: 'WeatherChartCard',
     sourcemap: dev ? true : false,
